cb0db3fb3f368ab9838da488f97d60e1bfbb11d4
tolutronics <toluxy100@gmail.com>
2020-11-16 10:52:25 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: true,
     firebase: {
-      apiKey: "AIzaSyCp6NIRWx3lWhcmucFwaKUjoLCIKUFhD44",
-      authDomain: "crytaldoms.firebaseapp.com",
-      databaseURL: "https://crytaldoms.firebaseio.com",
-      projectId: "crytaldoms",
-      storageBucket: "crytaldoms.appspot.com",
-      messagingSenderId: "112463456312"
+   
   }
 };
 
